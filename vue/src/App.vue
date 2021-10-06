985d9a817e79f583580e316730bac2e8b71e3731
Shumpei Asahi <a.s.mail071275@gmail.com>
2021-10-06 09:34:55 +0900

    211006

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <el-menu :default-active="activeIndex" mode="horizontal" router>
-      <el-menu-item index="home" :route="{ name:'home' }">Home</el-menu-item>
-      <el-menu-item index="about" :route="{ name:'about' }">About</el-menu-item>
+      <el-menu-item index="home" :route="{ name:'Home' }">Home</el-menu-item>
+      <el-menu-item index="about" :route="{ name:'About' }">About</el-menu-item>
       <el-menu-item>
         <a href="https://element.eleme.io" target="_blank">Link</a>
       </el-menu-item>
