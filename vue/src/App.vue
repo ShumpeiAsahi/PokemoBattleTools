b9ebd142f8760b07f5d00a57274e7127fbd8c58b
Shumpei Asahi <a.s.mail071275@gmail.com>
2021-10-14 08:50:43 +0900

    211014

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -30,3 +30,15 @@
   color: #42b983;
 }
 </style>
+<style lang="scss">
+@import "../node_modules/bulma/bulma.sass";
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
